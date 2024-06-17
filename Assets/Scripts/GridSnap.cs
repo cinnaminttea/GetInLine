85485af9816e95886517f8e75621d4525f0b82a6
calebxmarshall14 <130993605+dumbwaifu-sal@users.noreply.github.com>
2024-06-17 21:59:20 +0200

    Available movement highlight p1

--- Assets/Scripts/GridSnap.cs
+++ Assets/Scripts/GridSnap.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GridSnap : MonoBehaviour
@@ -7,16 +8,27 @@ public class GridSnap : MonoBehaviour
     public int gridSizeX = 15;
     public int gridSizeY = 13;
     public Vector3[,] gridArray;
+
+
     void Start()
     {
         gridArray = new Vector3[gridSizeX, gridSizeY];
         gridArray[0, 0] = new Vector3(-5.75f, 4.11f, 0);
     }
+    
 
-    // Update is called once per frame
     void Update()
     {
+
         //get coordinates of bead; find closest array position; snap to position
+        if (gameObject.GetComponent < beadMovement> ().canMove == true)
+        {
+
+            if (Input.GetMouseButtonUp (0))
+            {
+                Vector3 bead = gameObject.transform.position;
+            }
+        }
 
     }
 }
