683a79a74d1cf864a7cc18d071b4df9bdc366ac2
cinnaminttea <166618896+cinnaminttea@users.noreply.github.com>
2024-06-17 23:28:51 +0200

    Revert "Highlight p3"
    This reverts commit 4f5781841b8ec6a5602cc9dc8df61b224985693e.

--- Assets/Scripts/mouseClickRaycast.cs
+++ Assets/Scripts/mouseClickRaycast.cs
@@ -4,17 +4,10 @@ using UnityEngine.UIElements;
 public class mouseClickRaycast : MonoBehaviour
 {
     public Vector3 clickedBead;
-    public Vector3 highlightArea;
     public GameObject highlight;
     public Canvas canvas;
-    public Rect creationArea1;
-    public Rect creationArea2;
+    public Rect creationArea;
 
-    private void Start()
-    {
-        creationArea1 = new Rect(0, 0, 80.006f, 30.015f);
-        creationArea2 = new Rect(0, 0, 40.311f, 60.634f);
-    }
     // Update is called once per frame
     void Update()
     {
@@ -40,13 +33,10 @@ public class mouseClickRaycast : MonoBehaviour
                         for (int j = 0; j < 3; j++)
                         {
 
-                        highlightArea = clickedBead + new Vector3(0.702f * (i - 1), 0.694f * (j - 1), 0);
-
-                        Debug.Log(highlightArea.ToString());
-                        //if (creationArea1.Contains(highlightArea) || creationArea2.Contains(highlightArea))
-                            if (highlightArea.x>-4.5 && highlightArea.x < 4.5 && highlightArea.y > -4 && highlightArea.y < 4)
+                        //if (creationArea.Contains(clickedBead))
+                        if (1==1)
                             {
-                                Instantiate(highlight, highlightArea, Quaternion.identity, canvas.transform);
+                                Instantiate(highlight, clickedBead + new Vector3(0.702f * (i - 1), 0.694f * (j - 1), 0), Quaternion.identity, canvas.transform);
                             }
                         }
 
